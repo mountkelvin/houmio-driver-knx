67afdb8a26dfd166a7286e9f851354754a7a51c3
Jakub Järvenpää <jakub.jarvenpaa@reaktor.fi>
2015-02-05 04:28:11 +0200

    First stab at knx and knx/dali via two separate sockets with backpressure

--- src/houmio/KnxCommandData.java
+++ src/houmio/KnxCommandData.java
@@ -7,14 +7,18 @@ public class KnxCommandData {
     public final boolean on;
     public final int scaledBri;
     public final String switchAddress;
+    public final String switchConfirmationAddress;
     public final String absoluteAddress;
+    public final String absoluteConfirmationAddress;
 
-    public KnxCommandData(String type, boolean on, int scaledBri, String switchAddress, String absoluteAddress) {
+    public KnxCommandData(String type, boolean on, int scaledBri, String switchAddress, String switchConfirmationAddress, String absoluteAddress, String absoluteConfirmationAddress) {
         this.type = type;
         this.on = on;
         this.scaledBri = scaledBri;
         this.switchAddress = switchAddress;
+        this.switchConfirmationAddress = switchConfirmationAddress;
         this.absoluteAddress = absoluteAddress;
+        this.absoluteConfirmationAddress = absoluteConfirmationAddress;
     }
 
     public static KnxCommandData fromJson(JsonNode n) {
@@ -23,15 +27,15 @@ public class KnxCommandData {
         int bri = n.get("bri").asInt();
         int knxScaledBri = Scale.scale(0, 0xff, 0, 100, bri);
         String protocolAddress = n.get("protocolAddress").asText();
-        String[] groupAddresses = protocolAddress.split(":");
-        String switchAddress = "";
+        String[] groupAddresses = protocolAddress.split("\\.");
+        String switchAddress = groupAddresses[0];
+        String switchConfirmationAddress = groupAddresses[1];
         String absoluteAddress = "";
-        if (groupAddresses.length > 0) {
-            switchAddress = groupAddresses[0];
+        String absoluteConfirmationAddress = "";
+        if("dimmable".equals(type)) {
+            absoluteAddress = groupAddresses[2];
+            absoluteConfirmationAddress = groupAddresses[3];
         }
-        if (groupAddresses.length > 1) {
-            absoluteAddress = groupAddresses[1];
-        }
-        return new KnxCommandData(type, on, knxScaledBri, switchAddress, absoluteAddress);
+        return new KnxCommandData(type, on, knxScaledBri, switchAddress, switchConfirmationAddress, absoluteAddress, absoluteConfirmationAddress);
     }
 }
--- src/houmio/Main.java
+++ src/houmio/Main.java
@@ -1,12 +1,11 @@
 package houmio;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import io.reactivex.netty.RxNetty;
-import io.reactivex.netty.channel.ObservableConnection;
-import io.reactivex.netty.client.RxClient;
-import io.reactivex.netty.pipeline.PipelineConfigurators;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import rx.Observable;
+import rx.Subscriber;
 import rx.functions.Action1;
+import rx.functions.Func1;
+import rx.observables.StringObservable;
 import tuwien.auto.calimero.CloseEvent;
 import tuwien.auto.calimero.FrameEvent;
 import tuwien.auto.calimero.GroupAddress;
@@ -18,57 +17,113 @@ import tuwien.auto.calimero.link.medium.TPSettings;
 import tuwien.auto.calimero.process.ProcessCommunicator;
 import tuwien.auto.calimero.process.ProcessCommunicatorImpl;
 
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.SocketException;
-import java.net.UnknownHostException;
+import java.io.*;
+import java.net.*;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
-    static String knxIp = Optional.ofNullable(System.getenv("HOUMIO_KNX_IP")).orElse("localhost");
-    static Optional<String> networkInterface = Optional.ofNullable(System.getenv("HOUMIO_NETWORK_INTERFACE"));
+    static Optional<String> DEV = Optional.ofNullable(System.getenv("DEV"));
+    static String HOUMIO_KNX_IP = Optional.ofNullable(System.getenv("HOUMIO_KNX_IP")).orElse("localhost");
+    static Optional<String> HOUMIO_NETWORK_INTERFACE = Optional.ofNullable(System.getenv("HOUMIO_NETWORK_INTERFACE"));
+    static String HOUMIO_KNX_DALI_THROTTLE = Optional.ofNullable(System.getenv("HOUMIO_KNX_DALI_THROTTLE")).orElse("500");
     static KNXNetworkLinkIP knxLink;
     static ProcessCommunicator knxSocket;
 
-    public static void main(String[] args) throws SocketException, UnknownHostException, KNXException, InterruptedException {
-        System.out.println("Using HOUMIO_KNX_IP=" + knxIp);
-        networkInterface.ifPresent(i -> System.out.println("Using HOUMIO_NETWORK_INTERFACE=" + i));
-        knxSocket = createKnxSocket();
-        openTcpSocketToBridgeWithProtocolKnx();
-        openTcpSocketToBridgeWithProtocolKnxDali();
+    public static void main(String[] args) throws IOException, KNXException, InterruptedException {
+        System.out.println("Using HOUMIO_KNX_IP=" + HOUMIO_KNX_IP);
+        HOUMIO_NETWORK_INTERFACE.ifPresent(i -> System.out.println("Using HOUMIO_NETWORK_INTERFACE=" + i));
+        System.out.println("Using HOUMIO_KNX_DALI_THROTTLE=" + HOUMIO_KNX_DALI_THROTTLE);
+        setupKnxSocket();
+        setupBridgeSocketKnxDaliThread().start();
+        setupBridgeSocketKnxThread().start();
     }
 
-    private static void openTcpSocketToBridgeWithProtocolKnxDali() {
-        RxClient<String, String> rxClient = RxNetty.createTcpClient("localhost", 3001, PipelineConfigurators.stringMessageConfigurator());
-        Observable<ObservableConnection<String, String>> connectionObservable = rxClient.connect();
-        connectionObservable
-            .flatMap(c -> {
-                Observable<JsonNode> write = c.writeAndFlush(DriverProtocol.driverReadyMessage("knx/dali")).map(x -> Json.mapper.createObjectNode());
-                Observable<JsonNode> jsons = c.getInput().flatMap(Json.parseJson);
-                return Observable.concat(write, jsons);
-            })
-            .filter(json -> json.get("command").asText().equals("write"))
-            .map(o -> o.get("data"))
-            .map(KnxCommandData::fromJson)
-            .toBlocking()
-            .forEach(System.out::println);
+    private static Thread setupBridgeSocketKnxDaliThread() throws IOException {
+        return new Thread(() -> {
+            try {
+                createBridgeSocketCommandObservable("knx/dali")
+                    .flatMap(toForceOn)
+                    .onBackpressureBuffer()
+                    .subscribe(new KnxDaliCommandSubscriber());
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+        });
     }
 
-    private static void openTcpSocketToBridgeWithProtocolKnx() {
-        RxClient<String, String> rxClient = RxNetty.createTcpClient("localhost", 3001, PipelineConfigurators.stringMessageConfigurator());
-        Observable<ObservableConnection<String, String>> connectionObservable = rxClient.connect();
-        connectionObservable
-            .flatMap(c -> {
-                Observable<JsonNode> write = c.writeAndFlush(DriverProtocol.driverReadyMessage("knx")).map(x -> Json.mapper.createObjectNode());
-                Observable<JsonNode> jsons = c.getInput().flatMap(Json.parseJson);
-                return Observable.concat(write, jsons);
-            })
+    private static Func1<KnxCommandData, Observable<? extends KnxCommandData>> toForceOn = d -> {
+        if (d.on) {
+            KnxCommandData forceOn = new KnxCommandData("binary", true, 0, d.switchAddress, "", "", "");
+            return Observable.just(forceOn, d);
+        } else {
+            return Observable.just(d);
+        }
+    };
+
+    private static Thread setupBridgeSocketKnxThread() throws IOException {
+        return new Thread(() -> {
+            try {
+                createBridgeSocketCommandObservable("knx").forEach(writeToKnxBus);
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+        });
+    }
+
+    private static Observable<KnxCommandData> createBridgeSocketCommandObservable(String protocol) throws IOException {
+        Socket bridgeSocket = new Socket("localhost", 3001);
+        PrintWriter bridgeSocketWriter = new PrintWriter(bridgeSocket.getOutputStream());
+        bridgeSocketWriter.write(DriverProtocol.driverReadyMessage(protocol));
+        bridgeSocketWriter.flush();
+        return StringObservable.from(new BufferedReader(new InputStreamReader(bridgeSocket.getInputStream())))
+            .flatMap(Json.parseJson)
             .filter(json -> json.get("command").asText().equals("write"))
             .map(o -> o.get("data"))
-            .map(KnxCommandData::fromJson)
-            .toBlocking()
-            .forEach(writeToKnxBus);
+            .map(KnxCommandData::fromJson);
+    }
+
+    private static void setupKnxSocket() throws SocketException, KNXException, InterruptedException, UnknownHostException {
+        if (DEV.isPresent()) {
+            writeToKnxBus = knxCommandData -> {
+                try {
+                    System.out.println(Json.mapper.writeValueAsString(knxCommandData));
+                } catch (JsonProcessingException e) {
+                    e.printStackTrace();
+                }
+            };
+        } else {
+            knxSocket = createKnxSocket();
+        }
+    }
+
+    private static ProcessCommunicator createKnxSocket() throws SocketException, KNXException, InterruptedException, UnknownHostException {
+        InetAddress localInetAddress = resolveLocalInetAddress();
+        knxLink = new KNXNetworkLinkIP(
+            KNXNetworkLinkIP.TUNNELING,
+            new InetSocketAddress(localInetAddress, 0),
+            new InetSocketAddress(
+                InetAddress.getByName(HOUMIO_KNX_IP),
+                KNXnetIPConnection.DEFAULT_PORT),
+            false,
+            new TPSettings(false)
+        );
+        System.out.println(String.format("Connected to KNX IP gateway, from %s to %s", localInetAddress.getHostAddress(), HOUMIO_KNX_IP));
+        knxLink.addLinkListener(new LinkClosedListener());
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                System.out.println("Shut down hook execution started");
+                System.out.println("Going to close KNX link");
+                knxLink.close();
+                System.out.println("Shut down hook executed");
+            }
+        });
+        System.out.println("Shut down hook attached");
+        return new ProcessCommunicatorImpl(knxLink);
     }
 
     static Action1<KnxCommandData> writeToKnxBus = data -> {
@@ -97,46 +152,6 @@ public class Main {
         System.out.printf("Wrote to KNX, type: %s, address: %s, value: %b\n", "binary", switchAddress, on);
     }
 
-    private static ProcessCommunicator createKnxSocket() throws SocketException, KNXException, InterruptedException, UnknownHostException {
-        InetAddress localInetAddress = resolveLocalInetAddress();
-        knxLink = new KNXNetworkLinkIP(
-            KNXNetworkLinkIP.TUNNELING,
-            new InetSocketAddress(localInetAddress, 0),
-            new InetSocketAddress(
-                InetAddress.getByName(knxIp),
-                KNXnetIPConnection.DEFAULT_PORT),
-            false,
-            new TPSettings(false)
-        );
-        System.out.println(String.format("Connected to KNX IP gateway, from %s to %s", localInetAddress.getHostAddress(), knxIp));
-        knxLink.addLinkListener(new LinkClosedListener());
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-            @Override
-            public void run() {
-                System.out.println("Shut down hook execution started");
-                System.out.println("Going to close KNX link");
-                knxLink.close();
-                System.out.println("Shut down hook executed");
-            }
-        });
-        System.out.println("Shut down hook attached");
-        return new ProcessCommunicatorImpl(knxLink);
-    }
-
-    private static InetAddress resolveLocalInetAddress() throws SocketException {
-        List<InetAddress> inet4Addresses = NetworkInterfaces.getInet4Addresses(networkInterface);
-        if (inet4Addresses.size() == 1) {
-            InetAddress localInetAddress = inet4Addresses.get(0);
-            System.out.println("Local inet address is " + localInetAddress.getHostAddress());
-            return localInetAddress;
-        } else {
-            System.err.println("Multiple non-loopback inet v4 addresses present");
-            System.err.println("Specify network interface via environment variable HOUMIO_NETWORK_INTERFACE");
-            System.exit(0);
-            return null;
-        }
-    }
-
     private static class LinkClosedListener implements NetworkLinkListener {
         @Override
         public void confirmation(FrameEvent e) {
@@ -152,6 +167,47 @@ public class Main {
         public void linkClosed(CloseEvent e) {
             System.out.println("Connection closed to KNX IP gateway, reason: " + e.getReason());
             System.exit(0);
+        }
+
+    }
+
+    private static class KnxDaliCommandSubscriber extends Subscriber<KnxCommandData> {
+        @Override
+        public void onStart() {
+            request(1);
+        }
+
+        @Override
+        public void onCompleted() {
+
+        }
+
+        @Override
+        public void onError(Throwable throwable) {
+
+        }
+
+        @Override
+        public void onNext(KnxCommandData knxCommandData) {
+            writeToKnxBus.call(knxCommandData);
+            Observable.just(null).delay(Integer.parseInt(HOUMIO_KNX_DALI_THROTTLE), TimeUnit.MILLISECONDS).forEach(i -> {
+                request(1);
+            });
+        }
+
+    }
+
+    private static InetAddress resolveLocalInetAddress() throws SocketException {
+        List<InetAddress> inet4Addresses = NetworkInterfaces.getInet4Addresses(HOUMIO_NETWORK_INTERFACE);
+        if (inet4Addresses.size() == 1) {
+            InetAddress localInetAddress = inet4Addresses.get(0);
+            System.out.println("Local inet address is " + localInetAddress.getHostAddress());
+            return localInetAddress;
+        } else {
+            System.err.println("Multiple non-loopback inet v4 addresses present");
+            System.err.println("Specify network interface via environment variable HOUMIO_NETWORK_INTERFACE");
+            System.exit(0);
+            return null;
         }
     }
 }
