2c17cce0f8575fafeed666cc73d2766512ccb54b
Jakub Järvenpää <jakub.jarvenpaa@reaktor.fi>
2015-02-06 14:27:26 +0200

    Removed knx/dali stuff, process exits on timeouts and socket disconnects

--- src/houmio/Main.java
+++ src/houmio/Main.java
@@ -10,6 +10,7 @@ import tuwien.auto.calimero.CloseEvent;
 import tuwien.auto.calimero.FrameEvent;
 import tuwien.auto.calimero.GroupAddress;
 import tuwien.auto.calimero.exception.KNXException;
+import tuwien.auto.calimero.exception.KNXTimeoutException;
 import tuwien.auto.calimero.knxnetip.KNXnetIPConnection;
 import tuwien.auto.calimero.link.KNXNetworkLinkIP;
 import tuwien.auto.calimero.link.NetworkLinkListener;
@@ -24,58 +25,42 @@ import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
-    static Optional<String> DEV = Optional.ofNullable(System.getenv("DEV"));
     static String HOUMIO_KNX_IP = Optional.ofNullable(System.getenv("HOUMIO_KNX_IP")).orElse("localhost");
     static Optional<String> HOUMIO_NETWORK_INTERFACE = Optional.ofNullable(System.getenv("HOUMIO_NETWORK_INTERFACE"));
-    static String HOUMIO_KNX_DALI_THROTTLE = Optional.ofNullable(System.getenv("HOUMIO_KNX_DALI_THROTTLE")).orElse("500");
     static KNXNetworkLinkIP knxLink;
     static ProcessCommunicator knxSocket;
+    static Socket bridgeSocket;
 
     public static void main(String[] args) throws IOException, KNXException, InterruptedException {
-        System.out.println("Using HOUMIO_KNX_IP=" + HOUMIO_KNX_IP);
-        HOUMIO_NETWORK_INTERFACE.ifPresent(i -> System.out.println("Using HOUMIO_NETWORK_INTERFACE=" + i));
-        System.out.println("Using HOUMIO_KNX_DALI_THROTTLE=" + HOUMIO_KNX_DALI_THROTTLE);
-        setupKnxSocket();
-        setupBridgeSocketKnxDaliThread().start();
-        setupBridgeSocketKnxThread().start();
-    }
-
-    private static Thread setupBridgeSocketKnxDaliThread() throws IOException {
-        return new Thread(() -> {
-            try {
-                createBridgeSocketCommandObservable("knx/dali")
-                    .flatMap(toForceOn)
-                    .onBackpressureBuffer()
-                    .subscribe(new KnxDaliCommandSubscriber());
-            } catch (IOException e) {
-                e.printStackTrace();
-                System.exit(1);
-            }
-        });
-    }
-
-    private static Func1<KnxCommandData, Observable<? extends KnxCommandData>> toForceOn = d -> {
-        if (d.on) {
-            KnxCommandData forceOn = new KnxCommandData("binary", true, 0, d.switchAddress, "", "", "");
-            return Observable.just(forceOn, d);
-        } else {
-            return Observable.just(d);
+        try {
+            System.out.println("Using HOUMIO_KNX_IP=" + HOUMIO_KNX_IP);
+            HOUMIO_NETWORK_INTERFACE.ifPresent(i -> System.out.println("Using HOUMIO_NETWORK_INTERFACE=" + i));
+            knxSocket = createKnxSocket();
+            createBridgeSocketCommandObservable("knx").subscribe(new KnxCommandDataSubscriber());
+        } catch (KNXTimeoutException e) {
+            System.out.println("Error while connecting to KNX ip gateway: " + e.getMessage());
+        } catch (ConnectException e) {
+            System.out.println("Error while connecting to bridge: " + e.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-    };
-
-    private static Thread setupBridgeSocketKnxThread() throws IOException {
-        return new Thread(() -> {
-            try {
-                createBridgeSocketCommandObservable("knx").forEach(writeToKnxBus);
-            } catch (IOException e) {
-                e.printStackTrace();
-                System.exit(1);
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                System.out.println("Shut down hook execution started");
+                if(knxLink != null) {
+                    System.out.println("Going to close KNX link");
+                    knxLink.close();
+                    System.out.println("KNX link closed");
+                }
+                System.out.println("Shut down hook executed");
             }
         });
     }
 
     private static Observable<KnxCommandData> createBridgeSocketCommandObservable(String protocol) throws IOException {
-        Socket bridgeSocket = new Socket("localhost", 3001);
+        bridgeSocket = new Socket("localhost", 3001);
+        System.out.println("Connected to bridge at localhost:3001");
         PrintWriter bridgeSocketWriter = new PrintWriter(bridgeSocket.getOutputStream());
         bridgeSocketWriter.write(DriverProtocol.driverReadyMessage(protocol));
         bridgeSocketWriter.flush();
@@ -84,20 +69,6 @@ public class Main {
             .filter(json -> json.get("command").asText().equals("write"))
             .map(o -> o.get("data"))
             .map(KnxCommandData::fromJson);
-    }
-
-    private static void setupKnxSocket() throws SocketException, KNXException, InterruptedException, UnknownHostException {
-        if (DEV.isPresent()) {
-            writeToKnxBus = knxCommandData -> {
-                try {
-                    System.out.println(Json.mapper.writeValueAsString(knxCommandData));
-                } catch (JsonProcessingException e) {
-                    e.printStackTrace();
-                }
-            };
-        } else {
-            knxSocket = createKnxSocket();
-        }
     }
 
     private static ProcessCommunicator createKnxSocket() throws SocketException, KNXException, InterruptedException, UnknownHostException {
@@ -113,16 +84,6 @@ public class Main {
         );
         System.out.println(String.format("Connected to KNX IP gateway, from %s to %s", localInetAddress.getHostAddress(), HOUMIO_KNX_IP));
         knxLink.addLinkListener(new LinkClosedListener());
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-            @Override
-            public void run() {
-                System.out.println("Shut down hook execution started");
-                System.out.println("Going to close KNX link");
-                knxLink.close();
-                System.out.println("Shut down hook executed");
-            }
-        });
-        System.out.println("Shut down hook attached");
         return new ProcessCommunicatorImpl(knxLink);
     }
 
@@ -166,32 +127,29 @@ public class Main {
         @Override
         public void linkClosed(CloseEvent e) {
             System.out.println("Connection closed to KNX IP gateway, reason: " + e.getReason());
-            System.exit(0);
+            try {
+                bridgeSocket.close();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+                System.exit(1);
+            }
         }
     }
 
-    private static class KnxDaliCommandSubscriber extends Subscriber<KnxCommandData> {
-        @Override
-        public void onStart() {
-            request(1);
-        }
-
+    private static class KnxCommandDataSubscriber extends Subscriber<KnxCommandData> {
         @Override
         public void onCompleted() {
-
+            System.out.println("KNX command stream completed");
         }
 
         @Override
         public void onError(Throwable throwable) {
-
+            System.out.println("Error from KNX command stream: " + throwable.getMessage());
         }
 
         @Override
         public void onNext(KnxCommandData knxCommandData) {
             writeToKnxBus.call(knxCommandData);
-            Observable.just(null).delay(Integer.parseInt(HOUMIO_KNX_DALI_THROTTLE), TimeUnit.MILLISECONDS).forEach(i -> {
-                request(1);
-            });
         }
 
     }
